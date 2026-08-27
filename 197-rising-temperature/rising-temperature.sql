7b2749d17addd46bfa95a8da3c3e37987429c622
Vishwapratap Revande <revandeguru@gmail.com>
2026-08-27 09:28:11 +0530

    Time: 401 ms (91.98%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select w1.id 
+from 
+Weather w1
+INNER JOIN 
+Weather w2
+on 
+DATE_SUB(w1.recordDate,INTERVAL 1 DAY)=w2.recordDate
+where 
+(w1.temperature>w2.temperature);
