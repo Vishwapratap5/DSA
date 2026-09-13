c9b9d530f0fc591ad2dc7db7de862ff6d13aa6e5
Vishwapratap Revande <revandeguru@gmail.com>
2026-09-13 18:19:54 +0530

    Time: 5 ms (93.96%) | Memory: 82 MB (36.79%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.java
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.java
@@ -7,19 +7,19 @@ class Solution {
         int n=arr.length;
         long count=0;
         int sum=0;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        map.put(0,1);
-        map.put(1,0);
+        int evenCount=1;
+        int oddCount=0;
+        
 
         for(int end=0;end<n;end++){
             sum+=arr[end];
 
             if(sum%2!=0){
-                count+=map.get(0);
-                map.put(1,map.getOrDefault(1,0)+1);
+                count+=evenCount;
+                oddCount++;
             }else{
-                 count+=map.get(1);
-                map.put(0,map.getOrDefault(0,0)+1);
+                 count+=oddCount;
+                evenCount++;
             }
            
         }
