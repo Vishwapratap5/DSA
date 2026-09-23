222e2627acb362598b42db2e844aa5c4c054b07e
Vishwapratap Revande <revandeguru@gmail.com>
2026-09-23 22:56:25 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (61.78%) - LeetSync

---
+++ 1184-car-pooling/car-pooling.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int n=trips.length;
+        int max=0;
+        for(int i=0;i<n;i++){
+            max=Math.max(max,trips[i][2]);
+        }
+        int[] diff=new int[max+1];
+
+        for(int i=0;i<n;i++){
+           int l=trips[i][1];
+           int r=trips[i][2];
+           int x=trips[i][0];
+
+           diff[l]+=x;
+           diff[r]-=x;
+        }
+        int current=0;
+        int[] res=new int[max];
+        for(int i=0;i<max;i++){
+            current+=diff[i];
+            res[i]+=current;
+        }
+        for(int i=0;i<max;i++){
+            if(res[i]>capacity){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
